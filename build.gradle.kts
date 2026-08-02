d70a4c2d0de96088c0cb9c7d3a2d341c78e8247a
Donny Kurnia <donnykurnia+github@gmail.com>
2026-08-02 20:53:40 +0700

    fix: Add Enable-Native-Access attribute to jar manifest (#249)
    Add Enable-Native-Access: ALL-UNNAMED to the manifest of an executable
    JAR file.
    
    With this added, no need to add `--enable-native-access ALL-UNNAMED`
    flag when running the jar file
    
    ---------
    
    Co-authored-by: LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -221,7 +221,8 @@ tasks {
         manifest {
             attributes(
                 "Implementation-Title" to project.name,
-                "Implementation-Version" to project.version
+                "Implementation-Version" to project.version,
+                "Enable-Native-Access" to "ALL-UNNAMED"
             )
         }
     }
